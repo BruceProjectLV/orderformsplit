0e1a937deb0f637562d6bcda72f369170db2f3b2
Brucecode <59350885+BruceProjectLV@users.noreply.github.com>
2025-10-14 14:39:48 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 // Put your published Google Apps Script Web App URL here
 // Example: const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx.../exec";
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyHOijwjsk6vgmpIc2jRRUBiYfkQhDVFt2A1ajlNPUhNgO_CzMRrK3xR96Bi4jKIoLF/exec";
+window.SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyHOijwjsk6vgmpIc2jRRUBiYfkQhDVFt2A1ajlNPUhNgO_CzMRrK3xR96Bi4jKIoLF/exec";
