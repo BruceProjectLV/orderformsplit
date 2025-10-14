43f180382544f3b5abf0bc4d9174f18bf1b2e748
Brucecode <59350885+BruceProjectLV@users.noreply.github.com>
2025-10-13 19:37:19 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// Put your published Google Apps Script Web App URL here
+// Example: const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx.../exec";
+const SCRIPT_URL = "PASTE_YOUR_DEPLOYED_WEB_APP_URL_HERE";
